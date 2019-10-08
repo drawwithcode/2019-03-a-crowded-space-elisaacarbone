f48d451d0870b358ac6861be3ce002803335ad22
Elisa Carbone <carbone.elisa.98.ec@gmail.com>
2019-10-08 12:31:45 +0200

    def

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var fish = [];
 var badfish = [];
 var bubbles = [];
 
-var k = 0; //k is the variable that defines the mouseClicked function
+var k = 0; //k is the variable that defines the keyPressed function
 
 function preload() {
   bluu = loadFont('assets/BluuNext-Bolditalic.otf');
@@ -27,38 +27,48 @@ function setup() {
   }
 }
 
-//define the change when the mouse is clicked
-function mouseClicked(){
-  if(k==1){
+//define the change when keys are clicked
+function keyPressed(){
+  if(k==1 && keyCode === UP_ARROW){
     k=0;
-  }else if(k==0){
+  }else if(k==0 && keyCode === ENTER){
     k=1;
   }
   console.log(k);
 }
 
 function draw() {
-
-  //define what happens if the mouse is not clicked
+  //define what happens if the UP arrow is clicked or the initial condition
   if (k==0){
-    background("#33a0ff");
+    background("#66d9ff");
 
     //define the behaviour of the object Fish
     for (var i = 0; i < fish.length; i++) {
       fish[i].move();
       fish[i].display();}
 
-    //Click to go in bad waters txt
+    push();
+    stroke(0);
+    strokeWeight(1.5);
+    line(mouseX, 0, mouseX, mouseY);
+    fill(0)
+    triangle(mouseX + 15, mouseY, mouseX + 25, mouseY, mouseX + 20, mouseY - 10);
+    noFill();
+    arc(mouseX + 10, mouseY, 20, 20, 0, PI);
+    pop();
+
+    //press ENTER to go in bad waters txt
     push();
     textAlign(CENTER, CENTER);
     noStroke();
     fill("#001a1a");
     textSize(25);
     textFont(bluu);
-    text('Click to go in bad waters', windowWidth / 2, windowHeight - 30);
+    text('Click on the fish to catch them', windowWidth / 2, windowHeight - 60);
+    text('or press ENTER to go in bad waters', windowWidth / 2, windowHeight - 30);
     pop();
 
-  } //define what happens if the mouse is clicked
+  } //define what happens if the ENTER key is pressed
     else if (k==1) {
       background("#003333");
 
@@ -67,14 +77,25 @@ function draw() {
         badfish[j].move();
         badfish[j].display();}
 
-    //click to go back txt
+      push();
+      stroke(255);
+      strokeWeight(1.5);
+      line(mouseX, 0, mouseX, mouseY);
+      fill(255)
+      triangle(mouseX + 15, mouseY, mouseX + 25, mouseY, mouseX + 20, mouseY - 10);
+      noFill();
+      arc(mouseX + 10, mouseY, 20, 20, 0, PI);
+      pop();
+
+    //press UP arrow to go back txt
       push();
       textAlign(CENTER, CENTER);
       noStroke();
       fill("#e6ffff");
       textSize(25);
       textFont(bluu);
-      text('F**k, go back!!!', windowWidth / 2, windowHeight - 30);
+      text('Hurry up!! Click on the fish to kill them', windowWidth / 2, windowHeight - 60);
+      text('or press the UP arrow to run away', windowWidth / 2, windowHeight - 30);
       pop();
 
     }
@@ -88,12 +109,22 @@ function draw() {
 
 }
 
+function mousePressed() {
+  for (var i = 0; i < fish.length; i++) {
+    fish[i].clicked();}
+  for (var i = 0; i < badfish.length; i++) {
+    badfish[i].clicked();}
+}
+
 //orange Fish
 function Fish() {
   var a = 50;
   var b = 30;
   var c = 50;
   var d = 30;
+  this.colorbody = color("#ff6600");
+  this.coloreye = color(255);
+  this.coloriris = color(0);
 
   //define parameters of the object Fish
   this.x = random(-a, windowWidth);
@@ -122,13 +153,23 @@ function Fish() {
   //define the appearence of Fish
   this.display = function() {
     noStroke();
-    fill("#ff6600");
+    fill(this.colorbody);
     ellipse(this.x, this.y, this.diameter1, this.diameter2); //body
     triangle(this.x - c / 2, this.y, this.x - 2 * c / 3, this.y - d / 3, this.x - 2 * c / 3, this.y + d / 3); //tail
-    fill(255);
+    fill(this.coloreye);
     ellipse(this.x + c / 4, this.y - d / 10, c / 4); //eyeball white
-    fill(0);
+    fill(this.coloriris);
     ellipse(this.x + c / 4, this.y - d / 10, c / 7); //eyeball black
+  }
+
+  //define what happens when you click on a single object
+  this.clicked = function() {
+    var d = dist(mouseX, mouseY, this.x, this.y)
+    if (d < b/2) {
+      this.colorbody = color(0, 0);
+      this.coloreye = color(255,0);
+      this.coloriris = color(0,0);
+    }
   }
 
 }
@@ -136,7 +177,7 @@ function Fish() {
 //green fish
 function Badfish() {
   var e = 50;
-  var f = 30;
+  var f = 40;
   var g = 50;
   var h = 30;
 
@@ -146,6 +187,9 @@ function Badfish() {
   this.diameter1 = e;
   this.diameter2 = f;
   this.speed = 3;
+  this.colorbody = color("#177f70");
+  this.coloreye = color(255);
+  this.coloriris = color(0);
 
   var xIncrease = 2;
   var yIncrease = 0;
@@ -168,18 +212,27 @@ function Badfish() {
   this.display = function() {
     push();
     noStroke();
-    fill("#177f70");
+    fill(this.colorbody);
     ellipse(this.x, this.y, this.diameter1, this.diameter2); //body
     triangle(this.x - g / 2, this.y, this.x - 2 * g / 3, this.y - h / 3, this.x - 2 * g / 3, this.y + h / 3); //tail
-    fill(255);
+    fill(this.coloreye);
     ellipse(this.x + g / 4, this.y - h / 10, g / 4); //eyeball white
-    fill(0);
+    fill(this.coloriris);
     ellipse(this.x + g / 4, this.y - h / 10, e / 7); //eyeball black
     pop();
     strokeWeight(3);
-    stroke(0);
+    stroke(this.coloriris);
     line(this.x + g/4, this.y - h + 21, this.x + g/4 + 5, this.y - h + 23); //eyebrow1
     line(this.x + g/4 - 5, this.y - h + 23, this.x + g/4, this.y - h + 21); //eyebrow2
+  }
+
+  this.clicked = function() {
+    var d = dist(mouseX, mouseY, this.x, this.y)
+    if (d < e/2) {
+      this.colorbody = color(0, 0);
+      this.coloreye = color(255,0);
+      this.coloriris = color(0,0);
+    }
   }
 
 }
